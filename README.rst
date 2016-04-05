420d70e266a907778470690e5a9ead4de871ddfb
Antoine Dumas <dumas@phimeca.com>
2016-04-05 17:07:05 +0200

    Readme and doc change

--- README.rst
+++ README.rst
@@ -10,5 +10,10 @@ More information can found at http://www.openturns.org
 Installation
 ============
 
+In terminal, type in :
+$ python setup.py install
+
+Documentation is available in html at ./doc/build/html/index.html and in pdf at
+./doc/build/latex/otpoddoc.pdf
 
 -- The OpenTURNS team
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -235,8 +235,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'test.tex', u'test Documentation',
-     u'moi', 'manual'),
+    (master_doc, 'otpoddoc.tex', u'otpod Documentation',
+     u'Antoine Dumas', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -265,7 +265,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'test', u'test Documentation',
+    (master_doc, 'otpoddoc', u'otpod Documentation',
      [author], 1)
 ]
 
@@ -279,8 +279,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'test', u'test Documentation',
-     author, 'test', 'One line description of project.',
+    (master_doc, 'otpoddoc', u'otpod Documentation',
+     author, 'otpod', 'One line description of project.',
      'Miscellaneous'),
 ]
 

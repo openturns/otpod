c62c53d8e5b6b6f721f2ae34096863209c2ee708
Antoine Dumas <dumas@phimeca.com>
2016-04-06 15:18:55 +0200

    linear analysis - function run

--- otpod/_math_tools.py
+++ otpod/_math_tools.py
@@ -5,29 +5,24 @@ __all__ = []
 
 import openturns as ot
 import math as m
-from numpy import array, linspace, where
+import numpy as np
 
 
 
-#
+######### ReducedLogLikelihood #########
+# This class defines the log likelihood of the residuals versus lambda when
+# sigma and beta have been replaced by their expression in function of lambda
+# and the data.
 # LL(Y_i, a_i; \lambda, \sigma, \beta_0, \beta_1) =
 # -N/2\log(2\pi)-N\log(\sigma)-1/(2\sigma^2)\sum_{i=1}^N(T_{\lambda}(Y_i)-(\beta_0+\beta_1 a_i))^2
-
+#
+# Parameters
+# ----------
+# sigma2 : float
+#     unbiased estimate of the residual variance.
+# beta : sequence of float of dim = 2
+#     Least squares estimate of the linear regression model.
 class ReducedLogLikelihood(ot.OpenTURNSPythonFunction):
-
-    """
-    ReducedLogLikelihood
-    This class defines the log likelihood of the residuals versus lambda when
-    sigma and beta have been replaced by their expression in function of lambda
-    and the data.
-
-    Parameters:
-    -----------
-    sigma2 : float
-        unbiased estimate of the residual variance.
-    beta : sequence of float of dim = 2
-        Least squares estimate of the linear regression model.
-"""
 
     def __init__(self, a_i, Y_i):
         super(ReducedLogLikelihood, self).__init__(1, 1)
@@ -48,52 +43,147 @@ class ReducedLogLikelihood(ot.OpenTURNSPythonFunction):
         return [-0.5 * self.N_ * m.log(sigma2) + (Lambda[0] - 1) * self.sumLogY_i]
 
 ######### LinearBoxCoxFactory #########
-# Cette classe construit la transformation de Box-Cox optimale correspondant
-# a des residus Gaussiens une fois supprimee une tendance affine.
+# This class build the Box Cox optimal transformation corresponding with
+# Gaussian residuals once removes the linear trend.
 # Given data (a_i, Y_i), find the quadruplet (\lambda, \sigma, \beta_0, \beta_1
-# that maximize the log-likelihood function defined by ReducedLogLikelihood
+# that maximize the log-likelihood function defined by ReducedLogLikelihood.
 class LinearBoxCoxFactory:
 
     def __init__(self, lambdaMin = -3, lambdaMax = 3):
         self.lambdaMin_ = lambdaMin
         self.lambdaMax_ = lambdaMax
-        ot.Log.Show(Log.NONE)
+        ot.Log.Show(ot.Log.NONE)
         
     def build(self, dataX, dataY):
         logLikelihood = ot.NumericalMathFunction(ReducedLogLikelihood(dataX, dataY))
-        xlb = linspace(self.lambdaMin_,self.lambdaMax_,num=500)
+        xlb = np.linspace(self.lambdaMin_,self.lambdaMax_,num=500)
         lambdax = [logLikelihood([x])[0] for x in xlb]
         algo = ot.TNC(logLikelihood)
-        algo.setStartingPoint([xlb[array(lambdax).argmax()]])
+        algo.setStartingPoint([xlb[np.array(lambdax).argmax()]])
         algo.setBoundConstraints(ot.Interval(self.lambdaMin_, self.lambdaMax_))
         algo.setOptimizationProblem(ot.BoundConstrainedAlgorithmImplementationResult.MAXIMIZATION)
         algo.run()
         optimalLambda = algo.getResult().getOptimizer()[0]
-        # Pour verifier qu'on a bien l'optimum
-        # optimalLogLikelihood = algo.getResult().getOptimalValue()
-        # graph = logLikelihood.draw(0.1 * optimalLambda, 10.0 * optimalLambda)
-        # c = Cloud([[optimalLambda, optimalLogLikelihood]])
-        # c.setColor("red")
-        # c.setPointStyle("circle")
-        # graph.add(c)
         return ot.BoxCoxTransform([optimalLambda])
 
 
-######### computeBoxCox ######### 
-# cette fonction fait la transformation de Box Cox sur le sdonnnes
-# le lambda est estime par max de vraisemblance sur les donnees initiales
+######### computeBoxCox #########
+# This function applies the Box Cox transformation on the data.
+def computeBoxCox(factors, valuesInit, detection=None, lambdaBoxCox=None):
+    # if no affine trend is considered
+    # if lambdaBoxCox is None:
+    #     myBoxCoxFactory = BoxCoxFactory()
+    #     myModelTransform = myBoxCoxFactory.build(valuesInit)
+    # else:
+    #     myModelTransform = ot.BoxCoxTransform([lambdaBoxCox])
 
-def computeBoxCox(myData, seuilData):
-    valuesInit = myData[:,1]
-    # Si on ne considere pas de tendance
-    # myBoxCoxFactory = BoxCoxFactory()
-    # myModelTransform = myBoxCoxFactory.build(valuesInit)
-    # Si on considere une tendance affine
-    factors = myData[:,0]
-    myBoxCoxFactory = LinearBoxCoxFactory()
-    myModelTransform = myBoxCoxFactory.build(factors, valuesInit)
-    lambdaBoxCox = myModelTransform.getLambda()[0]
-    seuilBC = myModelTransform([seuilData])[0]
+    # if an affine trend is considered
+    if lambdaBoxCox is None:
+        myBoxCoxFactory = LinearBoxCoxFactory()
+        myModelTransform = myBoxCoxFactory.build(factors, valuesInit)
+        lambdaBoxCox = myModelTransform.getLambda()[0]
+    else:
+        myModelTransform = ot.BoxCoxTransform([lambdaBoxCox])
+
     valuesBC = myModelTransform(valuesInit)
     valuesBC.setDescription(['BoxCox('+ valuesInit.getDescription()[0] + ')'])
-    return lambdaBoxCox, valuesBC, seuilBC
+    
+    if detection is not None:
+        detectionBoxCox = myModelTransform([detection])[0]
+        return lambdaBoxCox, valuesBC, detectionBoxCox
+    else:
+        return lambdaBoxCox, valuesBC
+
+######### computeZeroMeanTest #########
+# This function tests if the residuals have a zero mean
+def computeZeroMeanTest(residuals):
+    # Student test with hypothesis zero mean
+    mRes = residuals.computeMean()[0]
+    varRes = residuals.computeVariance()[0]
+    stderr = np.sqrt(varRes / residuals.getSize())
+    statistic = mRes / stderr
+    # two sided test
+    return 2 * ot.DistFunc.pStudent(residuals.getSize()-1, -np.abs(statistic))
+
+######### computeBreuschPaganTest #########
+# This function tests if the residuals are homoskedastics
+def computeBreuschPaganTest(x, residuals):
+    nx = x.getSize()
+    df = 1 # linear regression with 2 parameters -> degree of freedom = 2 - 1
+    residuals = np.array(residuals)
+    sigma2 = np.sum(residuals**2) / nx
+    # Studentized Breusch Pagan
+    w = residuals**2 - sigma2
+    linResidual = ot.LinearLeastSquares(x, w)
+    linResidual.run()
+    linModel = linResidual.getResponseSurface()
+    wpred = np.array(linModel(x))
+    # statistic Breusch Pagan
+    bp = nx * np.sum(wpred**2) / np.sum(w**2)
+    # return complementary cdf of central ChiSquare
+    return 1 - ot.DistFunc.pNonCentralChiSquare(df, 0, bp)
+
+######### computeHarrisonMcCabeTest #########
+# This function tests if the residuals are homoskedastics
+def computeHarrisonMcCabeTest(residuals, breakRatio=0.5, simulationSize=1000):
+    # Parameters:
+    # breakPoint : ratio of point to define the break, must be 0 < breakRatio< 1
+    nx = residuals.getSize()
+    residuals = np.array(residuals)
+    breakpoint = int(np.floor(breakRatio * nx))
+    # statistic Harrison McCabe
+    hmc = np.sum(residuals[:breakpoint]**2) /np.sum(residuals**2)
+
+    # pvalue computed by simulation
+    stat = np.zeros(simulationSize)
+    normalDist = ot.Normal()
+    for i in range(simulationSize):
+        xSampleNor = normalDist.getSample(nx)
+        xstand = np.array((xSampleNor - xSampleNor.computeMean()[0]) / \
+                  xSampleNor.computeStandardDeviation()[0, 0])
+        stat[i] = np.sum(xstand[:breakpoint]**2) / np.sum(xstand**2)
+
+    return np.mean(stat < hmc)
+
+######### computeDurbinWatsonTest #########
+# This function tests if the residuals have non autocorrelation
+def computeDurbinWatsonTest(x, residuals, hypothesis="Equal"):
+    # Parameters:
+    # hypothesis : string
+    #    "Equal" : hypothesis is autocorrelation is 0
+    #    "Less" : hypothesis is autocorrelation is less than 0
+    #    "Greater" : hypothesis is autocorrelation is greater than 0
+    nx = x.getSize()
+    dim = x.getDimension()
+    residuals = np.array(residuals)
+    # statistic Durbin Watson
+    dw = np.sum(np.diff(np.hstack(residuals))**2)/np.sum(residuals**2)
+
+    # Normal approxiimation of DW to compute the pvalue
+    X = ot.Matrix(nx, dim+1)
+    X[:, 0] = np.ones((nx, 1))
+    X[:, 1] = x
+    B = ot.Matrix(nx, dim+1)
+    B[0, 1] = x[0][0] - x[1][0]
+    B[nx-1, 1] = x[nx-1][0] - x[nx-2][0]
+    for i in range(nx - 2):
+        B[i+1, 1] = -x[i][0] + 2 * x[i+1][0] - x[i+2][0]
+
+    XtX = X.computeGram()
+    XBQt = ot.SquareMatrix(XtX.solveLinearSystem(B.transpose() * X))
+    P = 2 * (nx - 1) - XBQt.computeTrace()
+    XBTrace = ot.SquareMatrix(XtX.solveLinearSystem(B.computeGram(), False)).computeTrace()
+    Q = 2 * (3 * nx - 4) - 2 * XBTrace + ot.SquareMatrix(XBQt * XBQt).computeTrace()
+    dmean = P / (nx - (dim + 1))
+    dvar = 2.0 / ((nx - (dim + 1)) * (nx - (dim + 1) + 2)) * (Q - P * dmean)
+
+    # compute the pvalue with respect to hypothesis
+    # Default pvalue is for hypothesis == "Equal"
+    # complementary CDF of standard normal distribution
+    pValue = 2 * ot.DistFunc.pNormal((dw - dmean) / np.sqrt(dvar), True);
+    if hypothesis == "Less":
+        pValue = 1 - pValue / 2;
+    elif hypothesis == "Greater":
+        pValue = pValue / 2;
+
+    return pValue
--- otpod/_univariate_linear_model_analysis.py
+++ otpod/_univariate_linear_model_analysis.py
@@ -4,11 +4,14 @@
 __all__ = ['UnivariateLinearModelAnalysis']
 
 import openturns as ot
+from ._math_tools import computeBoxCox, computeZeroMeanTest, computeBreuschPaganTest, computeHarrisonMcCabeTest, computeDurbinWatsonTest
+from statsmodels.regression.linear_model import OLS
+import numpy as np
 
 class UnivariateLinearModelAnalysis():
 
     """
-    Build a linear regression model and perform hypothesis test on the residuals.
+    Linear regression analysis with residuals hypothesis tests.
     
     **Available constructors**
 
@@ -20,33 +23,132 @@ class UnivariateLinearModelAnalysis():
     Parameters
     ----------
     inputSample : 2-d sequence of float
-        Vector of the defects size, of dimension 1.
+        Vector of the defect sizes, of dimension 1.
     outputSample : 2-d sequence of float
         Vector of the signals, of dimension 1.
     noiseThres : float
-        Low censored value
+        Value for low censored data. Default is None.
+    saturationThres : float
+        Value for high censored data. Default is None
+    resDistFact : :py:class:`openturns.DistributionFactory`
+        Distribution hypothesis followed by the residuals. Default is 
+        :py:class:`openturns.NormalFactory`.
+    boxCox : bool or float
+        Enable or not the Box Cox transformation. If boxCox is a float, the Box
+        Cox transformation is enabled with the given value. Default is False.
     """
 
     def __init__(self, inputSample, outputSample, noiseThres=None,
                  saturationThres=None, resDistFact=ot.NormalFactory(),
                  boxCox=False):
 
-        self.inputSample = inputSample
-        self.outputSample = outputSample
-        self.noiseThres = noiseThres
-        self.saturationThres = saturationThres
-        self.resDistFact = resDistFact
-        self.boxCox = boxCox
+        self._inputSample = ot.NumericalSample(inputSample)
+        self._outputSample = ot.NumericalSample(outputSample)
+        self._noiseThres = noiseThres
+        self._saturationThres = saturationThres
+        self._resDistFact = resDistFact
+
+        # if Box Cox is a float the transformation is enabled with the given value
+        if type(boxCox) is float:
+            self._lambdaBoxCox = boxCox
+            self._boxCox = True
+        else:
+            self._lambdaBoxCox = None
+            self._boxCox = boxCox
+
+        self._size = inputSample.getSize()
+        self._dim = inputSample.getDimension()
+
+        # Assertions on parameters
+        assert (self._size >=3), "Not enough observations."
+        assert (self._size == outputSample.getSize()), \
+                "InputSample and outputSample must have the same size."
+        assert (self._dim == 1), "InputSample must be of dimension 1."
+        assert (outputSample.getDimension() == 1), "OutputSample must be of dimension 1."
+
 
     def run(self):
         """
-        Bla bla bla
+        Run the analysis :
 
-        Parameters
-        ----------
-        sdfs : float
-            dfsdf
-        oko : bool
-            ture
+        - Compute the Box Cox parameter if boxCox is True,
+        - Compute the transformed signals if boxCox is enabled,
+        - Build the univariate linear regression model on the data,
+        - Compute the residuals,
+        - Run all hypothesis tests.
+
         """
-        pass
+        
+        ###################### Box Cox transform ###############################
+        # Compute Box Cox if enabled
+        if self._boxCox:
+            if self._lambdaBoxCox is None:
+                # optimization required
+                self._lambdaBoxCox, self._signals = computeBoxCox(self._inputSample,
+                                                            self._outputSample)
+            else:
+                # no optimization for lambdaBoxCox
+                self._lambdaBoxCox, self._signals = computeBoxCox(self._inputSample,
+                                                            self._outputSample,
+                                                            lambdaBoxCox=self._lambdaBoxCox)
+        else:
+            self._signals = self._outputSample
+
+        ######################### Linear Regression model ######################
+        # Create the X matrix : [1, inputSample]
+        X = ot.NumericalSample(self._size, [1, 0])
+        X[:, 1] = self._inputSample
+        algoLinear = OLS(np.array(self._signals), np.array(X)).fit()
+        self._intercept = algoLinear.params[0]
+        self._slope = algoLinear.params[1]
+
+        # create the linear model as NumericalMathFunction
+        def model(x):
+            return [self._intercept + self._slope * x[0]]
+        LinearRegModel = ot.PythonFunction(1, 1, model)
+
+        
+        ############################ Residuals #################################
+        # get Residuals
+        self._residuals = ot.NumericalSample(np.vstack(algoLinear.resid))
+        self._residuals.setDescription(['Residuals'])
+
+        # compute residual distribution
+        self._resDist = self._resDistFact.build(self._residuals)
+
+        # get confidence interval at level 95%
+        # square array
+        self._confInt = algoLinear.conf_int(0.05)
+
+        ########################## Compute test ################################
+        # compute R2
+        val = ot.MetaModelValidation(self._inputSample, self._signals, LinearRegModel)
+        self._R2 = val.computePredictivityFactor()
+
+        # compute Anderson Darling test (normality test)
+        testAnderDar = ot.NormalityTest.AndersonDarlingNormal(self._residuals)
+        self._pValAnderDar = testAnderDar.getPValue()
+
+        # compute Cramer Von Mises test (normality test)
+        testCramVM = ot.NormalityTest.CramerVonMisesNormal(self._residuals)
+        self._pValCramVM = testCramVM.getPValue()
+
+        # compute zero residual mean test
+        self._pValZeroMean = computeZeroMeanTest(self._residuals)
+
+        # compute Kolomogorov test (fitting test)
+        testKol = ot.FittingTest.Kolmogorov(self._residuals, self._resDist, 0.95,
+                                            self._resDist.getParametersNumber())
+        self._pValKol = testKol.getPValue()
+
+        # compute Breusch Pagan test (homoskedasticity : constant variance)
+        self._pValBreuPag = computeBreuschPaganTest(self._inputSample,
+                                                    self._residuals)
+
+        # compute Breusch Pagan test (homoskedasticity : constant variance)
+        self._pValHarMcCabe = computeHarrisonMcCabeTest(self._residuals)
+
+        # compute Durbin Watson test (autocorrelation == 0)
+        self._pValDurWat = computeDurbinWatsonTest(self._inputSample,
+                                                   self._residuals)
+

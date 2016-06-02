8ae290876969ced9e3cf88288de84d362c16e5cf
Antoine Dumas <dumas@phimeca.com>
2016-06-02 15:31:12 +0200

    inverse initialize function and import

--- otpod/__init__.py
+++ otpod/__init__.py
@@ -7,25 +7,6 @@
     and polynomial chaos. Sensitivity analysis can be also be performed.
 """
 
-from ._univariate_linear_model_analysis import *
-from ._univariate_linear_model_pod import *
-from ._quantile_regression_pod import *
-from ._polynomial_chaos_pod import *
-from ._kriging_pod import *
-from ._pod_summary import *
-from ._math_tools import *
-
-__version__ = "0.0"
-
-__all__ = (_univariate_linear_model_analysis.__all__ +
-           _univariate_linear_model_pod.__all__ + 
-           _quantile_regression_pod.__all__ +
-           _polynomial_chaos_pod.__all__ +
-           _kriging_pod.__all__ +
-           _pod_summary.__all__ +
-           _math_tools.__all__)
-
-
 def _initializing():
     # check version of required modules
     from importlib import import_module
@@ -52,3 +33,24 @@ def _initializing():
     logger.setLevel(logging.INFO)
 
 _initializing()
+
+from ._univariate_linear_model_analysis import *
+from ._univariate_linear_model_pod import *
+from ._quantile_regression_pod import *
+from ._polynomial_chaos_pod import *
+from ._kriging_pod import *
+from ._adaptive_signal_pod import *
+from ._pod_summary import *
+from ._math_tools import *
+
+__version__ = "0.0"
+
+__all__ = (_univariate_linear_model_analysis.__all__ +
+           _univariate_linear_model_pod.__all__ + 
+           _quantile_regression_pod.__all__ +
+           _polynomial_chaos_pod.__all__ +
+           _kriging_pod.__all__ +
+           _adaptive_signal_pod.__all__ +
+           _pod_summary.__all__ +
+           _math_tools.__all__)
+

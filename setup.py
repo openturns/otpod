fd0460ee11b81c6d5b73836b5ad7bce5bbfac8e3
Julien Schueller <schueller@phimeca.com>
2020-02-26 14:49:31 +0100

    setup: Drop logging for py3k

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
  
 from setuptools import setup, find_packages
- 
+import sys
 
 # Get the version from __init__.py
 with open('otpod/__init__.py') as fid:
@@ -10,37 +10,39 @@ with open('otpod/__init__.py') as fid:
         if line.startswith('__version__'):
             version = line.strip().split()[-1][1:-1]
             break
- 
+
+install_requires=['statsmodels>=0.6.1',
+                  'numpy>=1.10.4',
+                  'scikit-learn>=0.17',
+                  'matplotlib>=1.5.1',
+                  'scipy>=0.17.0',
+                  'decorator>=4.0.9']
+if sys.version_info < (3, 0):
+    install_requires.append('logging')
+
 setup(
- 
     # library name
     name='otpod',
- 
+
     # code version
     version=version,
-    
+
     # list libraries to be imported
     packages=find_packages(),
- 
+
     author="Antoine Dumas",
- 
+
     # Votre email, sachant qu'il sera publique visible, avec tous les risques
     # que ça implique.
     author_email="dumas@phimeca.com",
- 
+
     # Descriptions
     description="Build Probability of Detection curve",
     long_description=open('README.rst').read(),
- 
+
     # List of dependancies
-    install_requires= ['statsmodels>=0.6.1',
-                       'numpy>=1.10.4',
-                       'scikit-learn>=0.17',
-                       'matplotlib>=1.5.1',
-                       'scipy>=0.17.0',
-                       'logging',
-                       'decorator>=4.0.9']
- 
+    install_requires=install_requires
+
     # Enable to take into account MANIFEST.in
     # include_package_data=True,
 )

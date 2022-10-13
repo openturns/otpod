b0d52b64d49c1aa0afee193fdbc663b29d0d6280
Julien Schueller <schueller@phimeca.com>
2022-10-13 09:34:59 +0200

    Set version number 0.6.9

--- otpod/__init__.py
+++ otpod/__init__.py
@@ -40,7 +40,7 @@ from ._pli_pod import *
 from ._pli import *
 from ._math_tools import *
 
-__version__ = "0.6.8"
+__version__ = "0.6.9"
 
 __all__ = (
     _univariate_linear_model_analysis.__all__
